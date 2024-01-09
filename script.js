f34e5e87d75d9a8b0d3b147d049b58eab3d3ad77
JLP-55 <jack.v.pekin@gmail.com>
2024-01-09 22:38:23 +1100

    cleaned up and deleted superfluous code. this is likely to be the final commit. All for loops have been implemented correctly and there is no need for individual variables targeting unique id's in the html any longer.

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ $(function () {
 
     var save = $(".saveBtn");
 	var todaysDate = dayjs().format("[Today is] D MMM, YYYY");
-	// var theTime = dayjs().format("H");
-    var theTime = 8;
+	var theTime = dayjs().format("H");
     var secondsLeft = 1;
 
     // This variable will be used to allocate CSS selectors to the html elements.
@@ -13,34 +12,6 @@ $(function () {
     // This variable will be used to put items in local storage.
     var textAreaAll = $("section").children().children("textarea");
     // console.log(textAreaAll);
-
-    // Targets each div element in the html with the corresponding id's.
-    var giveTime09 = $("#9");
-    var giveTime10 = $("#10");
-    var giveTime11 = $("#11");
-    var giveTime12 = $("#12");
-    var giveTime13 = $("#13");
-    var giveTime14 = $("#14");
-    var giveTime15 = $("#15");
-    var giveTime16 = $("#16");
-    var giveTime17 = $("#17");
-
-    // Each item within the array "timeBlock" is its own array,
-    // so it must be targeted correctly in the for loop.
-    var timeBlock = [
-        giveTime09,
-        giveTime10,
-        giveTime11,
-        giveTime12,
-        giveTime13,
-        giveTime14,
-        giveTime15,
-        giveTime16,
-        giveTime17,
-    ];
-
-    // This will display each index in the timeBlock array. 
-    // console.log(timeBlock);
 
     // Function to save information to local storage upon clicking the save button.
 	save.on("click", function () {
@@ -68,49 +39,25 @@ $(function () {
 
 	});
 
-    // The variable hourTime will compare the id of each giveTime variable,
-    // in the timeBlock array to the variable theTime.
-    // Using .getAttribute will get the value of the id for each giveTime variable,
-    // and then parseInt will return a value that is a number, not a string.
-    // parseInt parses a string, in this case the id attribute that has been selected,
-    // and then returns a number. So if id="9" then it will return a value of 9.
-    // (this is so you can compare hourTime to "id").
-    // for (var i = 0; i < timeBlock.length; i++) {
-    //     var hourTime = parseInt(timeBlock[i][0].getAttribute("id"));
-    //     if (hourTime < theTime) {
-    //         timeBlock[i][0].setAttribute("class", "past row time-block");
-    //     };
-    //     if (hourTime == theTime) {
-    //         timeBlock[i][0].setAttribute("class", "present row time-block");
-    //     };
-    //     if (hourTime > theTime) {
-    //         timeBlock[i][0].setAttribute("class", "future row time-block");
-    //     };
-
-    //     // console.log(timeBlock[i][0].getAttribute("id"));
-    //     // console.log(hourTime);
-    // };
-
-    for (var i = 0; i < divAll.length; i++) {
-        var hourTime = parseInt(divAll.eq(i).attr("id"));
-        if (hourTime < theTime) {
-            divAll.eq(i).attr("class", "past row time-block");
-            // timeBlock[i][0].setAttribute("class", "past row time-block");
-        };
-        if (hourTime == theTime) {
-            divAll.eq(i).attr("class", "present row time-block");
-            // timeBlock[i][0].setAttribute("class", "present row time-block");
-        };
-        if (hourTime > theTime) {
-            divAll.eq(i).attr("class", "future row time-block");
-            // timeBlock[i][0].setAttribute("class", "future row time-block");
-        };
-        console.log(hourTime);
-    };
-
-    // This function will render saved items from local storage to the page.
     function init() {
-
+        
+        // This for loop will give classes to the div elements, styling them with the appropriate content given what time it is.
+        for (var i = 0; i < divAll.length; i++) {
+            var hourTime = parseInt(divAll.eq(i).attr("id"));
+            if (hourTime < theTime) {
+                divAll.eq(i).attr("class", "past row time-block");
+            };
+            if (hourTime == theTime) {
+                divAll.eq(i).attr("class", "present row time-block");
+            };
+            if (hourTime > theTime) {
+                divAll.eq(i).attr("class", "future row time-block");
+            };
+            // console.log(hourTime);
+    
+        };
+        
+        // This for loop will render saved items from local storage to the page.
         for (var i = 0; i < textAreaAll.length; i++) {
             var itemDisp = localStorage.getItem(textAreaAll.eq(i).attr("id"));
             // $("textarea").eq(i).append(itemDisp);
